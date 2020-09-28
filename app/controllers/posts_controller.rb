592b8676bb8f34b474dd4283a06c07171e5011f3
ViolaCello <ViolaCelloNYC@gmail.com>
2020-09-28 11:50:30 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
   end
@@ -11,8 +26,11 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
 
     @post.update(post_params)
-
+    if @post.valid?
     redirect_to post_path(@post)
+    else
+      render :edit
+       end
   end
 
   private
